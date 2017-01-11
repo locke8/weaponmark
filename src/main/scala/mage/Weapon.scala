a544b8a408af4d4477bdb89e436e4978f134f916
locke8 <locke8@plb.pw>
2017-01-11 01:32:36 -0800

    refactored to utilize AttackResult.attack(), added require() expressions to validate constructor parameters, added documentation. renamed some parameters for improved clarity and to match terms used in the core rules (WW4600)

--- src/main/scala/mage/Weapon.scala
+++ src/main/scala/mage/Weapon.scala
@@ -1,42 +1,80 @@
 package mage
 
-class Weapon(
-  val name:           String,
-  hitDice:            Int,
-  dmgDice:            Int,
-  hitDiff:            Int = 6,
-  dmgDiff:            Int = 6,
-  val splitDmod1:     Int = -2,
-  val splitDmod2:     Int = -3,
-  val splitDmod3:     Int = -4,
-  val specialty:      Boolean = false,
-  val actionsInSplit: Int = 0,
-  val actionsPerTurn: Int = 1) {
+import com.typesafe.scalalogging.LazyLogging
 
-  override def toString = s"$name ($hitDice/$dmgDice/$hitDiff)"
+/**
+  * Models a weapon as needed for benchmarking purposes.
+  * It can be used to mount an attack() and determine how many usesPerTurn() may be attempted; note that a botch
+  * will abort any actions/attacks remaining in that round.
+  * The attack() method is instrumented for logging at the level of TRACE, use when debugging.
+  * Require expressions validate constructor parameters, remove from production compile with flag -Xdisable-assertions.
+  *
+  * @param name any name you want, spaces are ok
+  * @param hitDice the # of dice to roll when attacking (to see if a hit occurs)
+  * @param dmgDice the # of dice to roll when calculating damage (when a hit occurs)
+  * @param hitDiff the difficulty of scoring a hit, range 2 to 10, default=6
+  * @param dmgDiff the difficulty of scoring damage, range 2 to 10, default=6
+  * @param actionsPerTurn used (with multiActions) to compute attempted uses per turn, range 1 to 9, default=1
+  * @param multiActions used (with actionsPerTurn) to compute attempted uses per turn, range 0 and 2-3, default=0
+  * @param dicePenalty1 dice penalty for 1st multi-action. default=multiActions, range 0 to 9.
+  *                     Subtracted from hitDice when attacking
+  * @param dicePenalty2 dice penalty for 1st multi-action. default=multiActions, range 0 to 9.
+  *                     Subtracted from hitDice when attacking
+  * @param dicePenalty3 dice penalty for 1st multi-action. default=multiActions, range 0 to 9.
+  *                     Subtracted from hitDice when attacking
+  * @param specialty true if 10 earns bonus rolls, false otherwise. As described on page 117 of the core rules book.
+  *                  If you are specialized in the use of your weapon, every 10 you roll to hit yields a bonus roll.
+  *                  Additional rolls of 10 extend this effect. Rolling a 1 on the bonus roll cancels out the 10.
+  */
+final case class Weapon(
+    name:           String,
+    hitDice:        Int,
+    dmgDice:        Int,
+    hitDiff:        Int = 6,
+    dmgDiff:        Int = 6,
+    actionsPerTurn: Int = 1,
+    multiActions:   Int = 0,
+    dicePenalty1:   Int = 0,
+    dicePenalty2:   Int = 0,
+    dicePenalty3:   Int = 0,
+    specialty:      Boolean = false )
+  extends LazyLogging {
 
-  // returns # of hit successes or zero
-  private def hits(dice: Int, threshold: Int = 6, accum: Int = 0) : Int =
-    if (dice > 0) {
-      val r = DieResult.rollToHit(threshold, specialty)
-      hits(dice - 1, threshold, accum + r.value)
-    } else accum
+  override def toString = s"$name ($hitDice/$dmgDice/$hitDiff/$dmgDiff/$actionsPerTurn/$multiActions/$dicePenalty1/$dicePenalty2/$dicePenalty3/$specialty)"
 
-  // returns number of damage successes
-  private def damage(dice: Int, threshold: Int = dmgDiff, accum: Int = 0) : Int =
-    if (dice == 0) accum
-    else {
-      val r = DieResult.rollDamage(threshold)
-      damage(dice - 1, accum + r.value, threshold)
-    }
-
-  // returns tuple where first element is # of hits and second is # of
-  // damage successes. Note that damage will be 0 if hits < 1
-  def use(diceMod: Int = 0, threshold: Int = hitDiff) : (Int, Int) = {
-    val h = hits(hitDice + diceMod, threshold)
-    val d = if (h > 0) damage(dmgDice + h - 1) else 0
-    (h, d)
+  /** returns an AttackResult (which includes the hit and damage successes) */
+  def attack(diceMod: Int = 0, opponent: Opponent) : AttackResult = {
+    val dice = hitDice - diceMod
+    logger.trace(s"             Roll for Attack:  penalty dice=$diceMod remaining dice=$dice difficulty=$hitDiff")
+    val result = AttackResult.attack(dice, this, opponent)
+    logger.trace(s"             $result")
+    result
   }
 
-  def usesPerTurn = actionsPerTurn + actionsInSplit - (if (actionsInSplit > 0) 1 else 0)
+  /**
+    * @return Int - the actual # of times the weapon will attempt to be used each turn
+    * @note a botch will cancel any remaining uses for the current turn
+    */
+  def usesPerTurn: Int = actionsPerTurn + (if (multiActions > 1) multiActions - 1 else 0)
+
+  require(name.nonEmpty)
+  require(actionsPerTurn >  0 && (actionsPerTurn <= Weapon.maxActionsPerTurn))
+  require(multiActions   >= 0 && (multiActions   <= Weapon.maxMultiActions))
+  require(hitDice        >  0 && (hitDice        <= Weapon.maxHitDice))
+  require(dmgDice        >  0 && (dmgDice        <= Weapon.maxDamageDice))
+  require(hitDiff        >  1 && (hitDiff        <= Die.dieBase))
+  require(dmgDiff        >  1 && (dmgDiff        <= Die.dieBase))
+  require(dicePenalty1   >= 0 && (dicePenalty1   <= Weapon.maxMultiPenalty))
+  require(dicePenalty2   >= 0 && (dicePenalty1   <= Weapon.maxMultiPenalty))
+  require(dicePenalty3   >= 0 && (dicePenalty1   <= Weapon.maxMultiPenalty))
+}
+
+object Weapon {
+  val maxHitDice           = 20
+  val maxDamageDice        = 30
+  val maxActionsPerTurn    =  9
+  val maxMultiActions      =  3
+  val maxMultiPenalty      =  9
+  val baseHitDifficulty    =  6
+  val baseDamageDifficulty =  6
 }

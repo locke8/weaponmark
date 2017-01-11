d305795554aa8d7fb7b424d3918d2baf5b2c2e53
locke8 <locke8@plb.pw>
2017-01-11 02:00:37 -0800

    rewrote class and instrumented for detailed logging

--- src/main/scala/weaponmark/Benchmark.scala
+++ src/main/scala/weaponmark/Benchmark.scala
@@ -1,52 +1,93 @@
 package weaponmark
 
-import mage.{Success, Weapon, DieResult}
+import com.typesafe.scalalogging.LazyLogging
 import org.scalactic.TimesOnInt._
+import mage.{AttackBotched, AttackFailed, AttackSucceeded, Weapon, Opponent}
 
-case class Benchmark(
-  weapon:     Weapon,
-  verbose:    Boolean = false,
-  soakDice:   Int = 0,
-  iterations: Int = 1000000) {
+final case class Benchmark(weapon: Weapon, opponent: Opponent, turns: Int = 200000) extends LazyLogging {
 
-  private val results = BenchmarkResults(this)
+  require(turns <= Benchmark.maxKiloTurns * 1000)
+  // minimum turns not enforced to support debugging runs where only 100 or so may be needed or desired
 
-  // use/fire/swing the weapon once, return true if botch occurs
-  private def use(diceMod: Int = 0): Boolean = {
-    var (hits, damage) = weapon.use(diceMod)
-    if (hits > 0) {
-      soakDice times {
-        DieResult.rollDamage() match {
-          case Success(r) => damage -= 1
-          case _ =>
-        }
+  private val results = BenchmarkResults(weapon.usesPerTurn, turns)
+  private val border = "================================================================================"
+
+  private def logHeader() = {
+    logger.debug(border)
+    logger.debug(s"= Begin Benchmark for $weapon, $turns turns, ${weapon.usesPerTurn} attempted uses per turn")
+    logger.debug(s"=   actions=${weapon.actionsPerTurn}  multi-actions=${weapon.multiActions}")
+    if (weapon.multiActions > 1) logger.debug(s"=   dMod1=${weapon.dicePenalty1}  dMod2=${weapon.dicePenalty2}" +
+      (if (weapon.multiActions > 2) s"  dMod3=${weapon.dicePenalty3}" else ""))
+  }
+
+  private def logFooter() = {
+    logger.debug("= ")
+    logger.debug(s"= End Benchmark for $weapon, $turns turns, ${weapon.usesPerTurn} attempted uses per turn")
+  }
+
+  /**
+    * use/fire/swing the weapon once collecting results as statistics
+    * @return ''true'' on hit or miss (weapon was used) OR ''false'' if botch occurs
+    */
+  private def useWeapon(diceMod: Int = 0) : Boolean = {
+    val result = weapon.attack(diceMod, opponent)
+
+    // tally results
+    result match {
+      case AttackBotched(_, _) =>
+        results.incBotches()
+      case AttackFailed(_, _) =>
+        results.incMisses()
+      case AttackSucceeded(hits, damage) =>
+        results.incHitsBy(hits)
+        results.incDamageBy(damage)
+        if (damage < 1) results.incZeroDamage()
+    }
+    !result.isBotch
+  }
+
+  /** performs all weapon actions/usages for a single turn */
+  private def iterateOneTurn() = {
+    /** performs any specified regular actions, stops on botch, returns true on success or false if botch occurred */
+    def doNormalActions(times: Int, noBotch: Boolean = true) : Boolean =
+      if (times > 0 && noBotch)
+        doNormalActions(times - 1, useWeapon())
+      else noBotch
+    /** performs any specified multi-actions, stops on botch, returns true on success or false if botch occurred */
+    def doMultiActions() =
+      if (weapon.multiActions > 1) {
+        logger.trace(s"   Begin Multi-Action:  ${weapon.multiActions} separate actions")
+        useWeapon(weapon.dicePenalty1) &&
+          useWeapon(weapon.dicePenalty2) &&
+          ((weapon.multiActions < 3) || useWeapon(weapon.dicePenalty3))
       }
-      results.totHits += hits
-      results.totDamage += damage
-      if (damage == 0) results.tot0Damage += 1
-    }
-    else if (hits < 0) results.totBotches += 1
-    else results.totMisses += 1
-    hits < 0
+
+    val normalActions = weapon.actionsPerTurn - (if (weapon.multiActions > 1) 1 else 0)
+    logger.trace(s"  Begin New Turn:  normal_actions=$normalActions + multi_actions=${weapon.multiActions}")
+
+    // if a botch occurs during normal action(s), skip any multi-actions for this turn
+    if (doNormalActions(normalActions))
+      doMultiActions()
   }
 
-  // iterate over one turn worth of weapon use
-  private def iterate() = {
-    var botch = false
-    val uses = weapon.actionsPerTurn - (if (weapon.actionsInSplit > 0) 1 else 0)
-    uses times {
-      if (!botch) botch = use()
-    }
-    if (weapon.actionsInSplit > 0) {
-      if (!botch) botch = use(weapon.splitDmod1)
-      if (!botch) botch = use(weapon.splitDmod2)
-
-      if (!botch && (weapon.actionsInSplit > 2)) botch = use(weapon.splitDmod3)
-    }
-  }
-
-  def run() : BenchmarkResults = {
-    iterations times { iterate() }
+/**
+  * executes the benchmark for ''turns'' turns, logging activity and collecting stats/results as it goes
+  * @return BenchmarkResults chronicling the run of this benchmark
+ */
+  def run(): BenchmarkResults = {
+    logHeader()
+    turns times { iterateOneTurn() }
+    logFooter()
     results
   }
+
+  def printResults(): Unit = BenchmarkResultsPrinter(this, results).print()
+}
+
+object Benchmark {
+  val minKiloTurns = 200
+  val maxKiloTurns = 1000
+
+  /** returns a new (un-run) Benchmark using 'b' to supply the constructor parameters */
+  def newFrom(b: Benchmark): Benchmark = Benchmark(b.weapon, b.opponent, b.turns)
 }

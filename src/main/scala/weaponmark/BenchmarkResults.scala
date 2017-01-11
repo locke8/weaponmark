929d338d0cf7ea6395991815648112bf92fa8497
locke8 <locke8@plb.pw>
2017-01-11 02:09:11 -0800

    rewrote class, added require expressions to validate constructor parameters, removed dependency on Benchmark and ColoredPrinter. Moved print-related behavior to new class BenchmarkResultsPrinter

--- src/main/scala/weaponmark/BenchmarkResults.scala
+++ src/main/scala/weaponmark/BenchmarkResults.scala
@@ -1,68 +1,50 @@
 package weaponmark
 
-import com.diogonunes.jcdp.color.ColoredPrinter
-import com.diogonunes.jcdp.color.api.Ansi.{Attribute, BColor, FColor}
+final case class BenchmarkResults(
+                usesPerTurn: Int,
+                turns:       Int,
+    private var hits:        Int = 0,
+    private var misses:      Int = 0,
+    private var zeroDamage:  Int = 0,
+    private var botches:     Int = 0,
+    private var damage:      Int = 0) {
 
-sealed case class BenchmarkResults(
-  benchmark:      Benchmark,
-  var tot0Damage: Int = 0,
-  var totBotches: Int = 0,
-  var totMisses:  Int = 0,
-  var totHits:    Int = 0,
-  var totDamage:  Int = 0) {
+  // validate constructor parameters
+  require(usesPerTurn > 0)
+  require(turns > 0)
+  require(hits == 0)
+  require(misses == 0)
+  require(zeroDamage == 0)
+  require(botches == 0)
+  require(damage == 0)
 
-  private val weapon = benchmark.weapon
-  private val verbose = benchmark.verbose
-  private val soakDice = benchmark.soakDice
-  private val iterations = benchmark.iterations
-  private def totUses = weapon.usesPerTurn * iterations
-  private def hitsPerUse = totHits.toDouble / totUses
-  private def hitsPerTurn = totHits.toDouble / iterations
-  private def damPerUse = totDamage.toDouble / totUses
-  private def damPerTurn = totDamage.toDouble / iterations
-  private def pctMisses = (totMisses.toDouble / totUses) * 100
-  private def pctBotches = (totBotches.toDouble / totUses) * 100
-  private def pctZeroDam = (tot0Damage.toDouble / totUses) * 100
-  private def pctIneffective = pctMisses + pctBotches + pctZeroDam
-
-  private final val cp = new ColoredPrinter.Builder(1, false)
-    .foreground(FColor.WHITE).background(BColor.BLACK)
-    .build()
-
-  private def printTitle() = {
-    cp.clear()
-    cp.print(weapon.name + (if (weapon.specialty) " (specialized)" else ""), Attribute.CLEAR, FColor.CYAN, BColor.BLACK)
-    cp.clear()
-    cp.println(
-      ": " + weapon.usesPerTurn + " uses/turn" +
-      (if (weapon.actionsInSplit > 0) f" including a split-action-${weapon.actionsInSplit}" else "") +
-      (if (soakDice > 0) s" plus $soakDice soak dice"  else "") +
-      f" for $iterations%,8d turns:")
+  // mutators to increment counts
+  def incZeroDamage(): Unit = zeroDamage += 1
+  def incBotches(): Unit = botches += 1
+  def incMisses(): Unit = misses += 1
+  def incHitsBy(h : Int): Unit = {
+    require(h >= 0) // accumulators don't go backwards
+    hits += h
   }
-
-  private def printHeader() = {
-    cp.clear()
-    cp.setForegroundColor(FColor.GREEN)
-    cp.println("   Hits/Use   Hits/Turn   Dam/Use   Dam/Turn" +
-      (if (verbose) "     %Miss + %Botch + %0Damage = %Ineffective" else ""))
+  def incDamageBy(d : Int): Unit = {
+    require(d >= 0) // accumulators don't go backwards
+    damage += d
   }
+  // accessors for counts
+  def totalHits: Int       = hits
+  def totalMisses: Int     = misses
+  def totalZeroDamage: Int = zeroDamage
+  def totalBotches: Int    = botches
+  def totalDamage: Int     = damage
 
-  private def printDetail() = {
-    cp.clear()
-    cp.print(f" $hitsPerUse%10.2f $hitsPerTurn%11.2f $damPerUse%9.2f", Attribute.CLEAR, FColor.YELLOW, BColor.BLACK)
-    cp.print(f"$damPerTurn%11.2f", Attribute.BOLD, FColor.CYAN, BColor.BLACK)
-    if (verbose) {
-      cp.print(f"   $pctMisses%6.2f%% $pctBotches%7.2f%%$pctZeroDam%10.2f%%",
-        Attribute.CLEAR, FColor.YELLOW, BColor.BLACK)
-      cp.print(f" $pctIneffective%13.2f%%",Attribute.CLEAR, FColor.CYAN, BColor.BLACK)
-    }
-    cp.clear()
-    cp.println("")
-  }
-
-  def output() = {
-    printTitle()
-    printHeader()
-    printDetail()
-  }
+  private val attemptedUses = usesPerTurn * turns
+  // statistic accessors
+  def hitsPerTurn: Double    = hits.toDouble / turns
+  def damPerTurn: Double     = damage.toDouble / turns
+  def hitsPerUse: Double     = hits.toDouble / attemptedUses
+  def damPerUse: Double      = damage.toDouble / attemptedUses
+  def pctMisses: Double      = (misses.toDouble / attemptedUses) * 100
+  def pctBotches: Double     = (botches.toDouble / attemptedUses) * 100
+  def pctZeroDam: Double     = (zeroDamage.toDouble / attemptedUses) * 100
+  def pctIneffective: Double = pctMisses + pctBotches + pctZeroDam
 }

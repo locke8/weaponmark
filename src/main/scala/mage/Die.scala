4d79ca69a66025fff8ddb7249c4010bf87292b56
locke8 <locke8@plb.pw>
2017-01-13 02:54:54 -0800

    added documentation

--- src/main/scala/mage/Die.scala
+++ src/main/scala/mage/Die.scala
@@ -1,5 +1,11 @@
 package mage
 
+/**
+  * Models the result of rolling a single 10-sided die against a given 'difficulty'
+  *
+  * @param value the number (1-10) that was randomly rolled
+  * @param difficulty the value needed to determine success or failure
+  */
 final case class Die(value: Int, difficulty: Int = 6) {
   def isBotch:   Boolean = value == 1
   def isSpecial: Boolean = value == Die.dieBase
@@ -8,11 +14,17 @@ final case class Die(value: Int, difficulty: Int = 6) {
   require(difficulty > 1 && (difficulty <= Die.dieBase), s"difficulty must be > 1 and <= ${Die.dieBase}")
 }
 
+/** Models the rolling of a single 10-sided die */
 object Die {
   import scala.util.Random
   final val dieBase = 10
   private[this] val rnd = new Random
 
-  /** returns a Die with a random value between 1 and dieBase */
+  /**
+    * Rolls a 10-sided die against a supplied 'difficulty' and returns the results
+    *
+    * @param difficulty [optional] the value needed to determine success or failure, default value = 6
+    * @return an instance of Die with the value that was rolled and the difficulty it was rolled against
+    */
   def roll(difficulty: Int = 6) = Die(rnd.nextInt(dieBase) + 1, difficulty)
 }
